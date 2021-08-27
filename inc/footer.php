e77659b9d66bb5d4b04fb22e07e38cb6336b5b17
Babar Qureshi <56481798+Babar-Ahmed@users.noreply.github.com>
2021-08-27 16:21:58 -0700

    crud php
    crud php

---
+++ inc/footer.php
@@ -0,0 +1,18 @@
+</div>
+
+  <footer class="footeroption">
+	  <section class="footerone">
+	  <p>💻Babar Qureshi💻</p>
+	  <p>💓</p>
+	  <p>😍😍😍</p>
+	  
+	  </section>
+	  <section class="footerone">
+		  <p>##########</p>
+		  <p>FB : babarqureshi09 </p>
+		  <p>Web: <a href='https://babarqureshi.com' target=_blank>https://babarqureshi.com/</a></p>
+	  </section>
+  </footer>
+
+</body>
+</html>
